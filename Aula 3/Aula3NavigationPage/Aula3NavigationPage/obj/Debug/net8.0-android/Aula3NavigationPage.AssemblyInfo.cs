973681fdf5315b4c206fa861e30a5f120965e325
Tauani Vitória <tauanivitoria1302@gmail.com>
2024-03-26 17:43:08 -0300

    Aula 3 e 4

--- Aula 3/Aula3NavigationPage/Aula3NavigationPage/obj/Debug/net8.0-android/Aula3NavigationPage.AssemblyInfo.cs
+++ Aula 3/Aula3NavigationPage/Aula3NavigationPage/obj/Debug/net8.0-android/Aula3NavigationPage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula3NavigationPage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0aafcd99aa30fdfb36389ac036d0e19efcc39219")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula3NavigationPage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula3NavigationPage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
